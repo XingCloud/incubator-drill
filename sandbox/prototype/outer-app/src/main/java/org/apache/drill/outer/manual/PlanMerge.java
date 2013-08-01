7ff6d82310e265e71cf6c6373dccd3cd65d9a7a1
immars <immars@gmail.com>
2013-08-01 12:07:24 +0800

    initial commit for plan merge

---
+++ sandbox/prototype/outer-app/src/main/java/org/apache/drill/outer/manual/PlanMerge.java
@@ -0,0 +1,12 @@
+package org.apache.drill.outer.manual;
+
+import org.apache.drill.common.logical.LogicalPlan;
+
+import java.util.List;
+
+public class PlanMerge {
+  
+  public static List<LogicalPlan> sortAndMerge(List<LogicalPlan> plans){
+    return plans;
+  }
+}
