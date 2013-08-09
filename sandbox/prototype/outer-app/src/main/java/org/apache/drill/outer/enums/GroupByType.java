69fbd5222aef789ce42ba4b22759c3764ff647c9
witwolf <wo.bian.shi.mo0@gmail.com>
2013-08-09 19:24:50 +0800

    remove outer-app

--- sandbox/prototype/outer-app/src/main/java/org/apache/drill/outer/enums/GroupByType.java
+++
@@ -1,8 +0,0 @@
-package org.apache.drill.outer.enums;
-
-/**
- * User: Z J Wu Date: 13-7-16 Time: 上午10:17 Package: org.apache.drill.common.enums
- */
-public enum GroupByType {
-  EVENT, USER_PROPERTY, INTERNAL_FUNC
-}
--- sandbox/prototype/outer-app/src/main/java/org/apache/drill/outer/enums/TableType.java
+++
@@ -1,8 +0,0 @@
-package org.apache.drill.outer.enums;
-
-/**
- * User: Z J Wu Date: 13-7-12 Time: 上午10:26 Package: org.apache.drill.common.enums
- */
-public enum TableType {
-  EVENT, USER
-}
