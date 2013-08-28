a7c79d58dcc33c241f2ed6cd12f8f4d9a3eb9c0f
immars <immars@gmail.com>
2013-08-28 10:49:33 +0800

    change selections type

--- sandbox/prototype/common/src/main/java/org/apache/drill/common/logical/data/UnionedScan.java
+++ sandbox/prototype/common/src/main/java/org/apache/drill/common/logical/data/UnionedScan.java
@@ -8,35 +8,36 @@ import org.apache.drill.common.expression.FieldReference;
 import org.apache.drill.common.logical.data.visitors.LogicalVisitor;
 
 @JsonTypeName("unioned-scan")
-public class UnionedScan extends SourceOperator{
+public class UnionedScan extends SourceOperator {
   private final String storageEngine;
- 	private final JSONOptions[] selections;
- 	private final FieldReference[] outputReferences;
- 	
- 	@JsonCreator
-   public UnionedScan(@JsonProperty("storageengine") String storageEngine, @JsonProperty("selections") JSONOptions[] selections, @JsonProperty("refs") FieldReference[] outputReferences) {
-     super();
-     this.storageEngine = storageEngine;
-     this.selections = selections;
-     this.outputReferences = outputReferences;
-   }
- 
-   @JsonProperty("storageengine")
-   public String getStorageEngine() {
-     return storageEngine;
-   }
- 
-   public JSONOptions[] getSelections() {
-     return selections;
-   }
- 
-   @JsonProperty("ref")
-   public FieldReference[] getOutputReferences() {
-     return outputReferences;
-   }
- 
-   @Override
-   public <T, X, E extends Throwable> T accept(LogicalVisitor<T, X, E> logicalVisitor, X value) throws E {
-       return logicalVisitor.visitUnionedScan(this, value);
-   }
+  private final JSONOptions selection;
+  private final FieldReference outputReference;
+
+  @JsonCreator
+  public UnionedScan(@JsonProperty("storageengine") String storageEngine, @JsonProperty("selection") JSONOptions selection, @JsonProperty("ref") FieldReference outputReference) {
+    super();
+    this.storageEngine = storageEngine;
+    this.selection = selection;
+    this.outputReference = outputReference;
+  }
+
+  @JsonProperty("storageengine")
+  public String getStorageEngine() {
+    return storageEngine;
+  }
+
+  @JsonProperty("selection")
+  public JSONOptions getSelection() {
+    return selection;
+  }
+
+  @JsonProperty("ref")
+  public FieldReference getOutputReference() {
+    return outputReference;
+  }
+
+  @Override
+  public <T, X, E extends Throwable> T accept(LogicalVisitor<T, X, E> logicalVisitor, X value) throws E {
+    return logicalVisitor.visitUnionedScan(this, value);
+  }
 }
