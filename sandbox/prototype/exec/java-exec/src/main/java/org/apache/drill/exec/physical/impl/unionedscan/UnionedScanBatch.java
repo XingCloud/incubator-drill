ddb4de075431d01906fa277f084059d058af92ea
witwolf <wo.bian.shi.mo0@gmail.com>
2013-09-17 11:00:28 +0800

    union scan fix

--- sandbox/prototype/exec/java-exec/src/main/java/org/apache/drill/exec/physical/impl/unionedscan/UnionedScanBatch.java
+++ sandbox/prototype/exec/java-exec/src/main/java/org/apache/drill/exec/physical/impl/unionedscan/UnionedScanBatch.java
@@ -320,6 +320,8 @@ public class UnionedScanBatch implements RecordBatch {
         return IterOutcome.NONE;
       }
       if(lastReaderEntry < mySortedEntry){
+        if(outcome == IterOutcome.NONE)
+          return IterOutcome.NONE;
         //cannot reach this point!
         throw new IllegalStateException("reader:"+lastReaderEntry+" is behind this split:"+mySortedEntry);
       }
