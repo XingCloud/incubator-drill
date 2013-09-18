2c79fe2c76115c6c6865f2fc69652815b77c7327
immars <immars@gmail.com>
2013-09-18 19:42:56 +0800

    test method for DFA correctness test

--- sandbox/prototype/exec/java-exec/src/test/java/org/apache/drill/exec/store/TestParseRowkey.java
+++ sandbox/prototype/exec/java-exec/src/test/java/org/apache/drill/exec/store/TestParseRowkey.java
@@ -11,6 +11,7 @@ import org.apache.drill.common.expression.SchemaPath;
 import org.apache.drill.common.types.TypeProtos;
 import org.apache.drill.common.types.Types;
 import org.apache.drill.common.util.FileUtils;
+import org.apache.drill.exec.memory.BufferAllocator;
 import org.apache.drill.exec.memory.DirectBufferAllocator;
 import org.apache.drill.exec.record.MaterializedField;
 import org.apache.drill.exec.util.parser.DFARowKeyParser;
@@ -20,6 +21,7 @@ import org.apache.drill.exec.vector.TypeHelper;
 import org.apache.drill.exec.vector.ValueVector;
 import org.apache.hadoop.hbase.util.Bytes;
 import org.apache.hadoop.hbase.util.Pair;
+import org.junit.Assert;
 import org.junit.Before;
 import org.junit.Test;
 
@@ -63,44 +65,96 @@ public class TestParseRowkey {
       Map<String, Pair<Integer, Integer>> constField = dfaRowKeyParser.getConstField();
       assertEquals(3, constField.size());
     }
-
-  @Test
-  public void testParseRowKey(){
-    String rowKey = "20130918response.agei.report.241427s.pend.2s5s.\\xFF[\\x00\\x00\\x00@"  ;
+  
+  public void assertParseRowKey(String rowKey, long date, String event0, String event1, String event2, String event3, String event4, String event5, int uhash, int uid){
+    BufferAllocator allocator = new DirectBufferAllocator();
     Map<String, ValueVector> vvMap = new HashMap<>();
     MaterializedField f = MaterializedField.create(new SchemaPath("uid", ExpressionPosition.UNKNOWN), Types.required(TypeProtos.MinorType.INT));
-    ValueVector vv = TypeHelper.getNewVector(f, new DirectBufferAllocator());
+    ValueVector vv = TypeHelper.getNewVector(f, allocator);
     AllocationHelper.allocate(vv, BATCH_SIZE, 4);
     vvMap.put("uid", vv);
+    f = MaterializedField.create(new SchemaPath("uhash", ExpressionPosition.UNKNOWN), Types.required(TypeProtos.MinorType.UINT1));
+    vv = TypeHelper.getNewVector(f, allocator);
+    AllocationHelper.allocate(vv, BATCH_SIZE, 4);
+    vvMap.put("uhash", vv);
+    f = MaterializedField.create(new SchemaPath("date", ExpressionPosition.UNKNOWN), Types.required(TypeProtos.MinorType.INT));
+    vv = TypeHelper.getNewVector(f, allocator);
+    AllocationHelper.allocate(vv, BATCH_SIZE, 4);
+    vvMap.put("date", vv);
+
+    f = MaterializedField.create(new SchemaPath("event0", ExpressionPosition.UNKNOWN), Types.required(TypeProtos.MinorType.VARCHAR));
+    vv = TypeHelper.getNewVector(f, allocator);
+    AllocationHelper.allocate(vv, BATCH_SIZE, 4);
+    vvMap.put("event0", vv);
+    f = MaterializedField.create(new SchemaPath("event1", ExpressionPosition.UNKNOWN), Types.required(TypeProtos.MinorType.VARCHAR));
+    vv = TypeHelper.getNewVector(f, allocator);
+    AllocationHelper.allocate(vv, BATCH_SIZE, 4);
+    vvMap.put("event1", vv);
+    f = MaterializedField.create(new SchemaPath("event2", ExpressionPosition.UNKNOWN), Types.required(TypeProtos.MinorType.VARCHAR));
+    vv = TypeHelper.getNewVector(f, allocator);
+    AllocationHelper.allocate(vv, BATCH_SIZE, 4);
+    vvMap.put("event2", vv);
+    f = MaterializedField.create(new SchemaPath("event3", ExpressionPosition.UNKNOWN), Types.required(TypeProtos.MinorType.VARCHAR));
+    vv = TypeHelper.getNewVector(f, allocator);
+    AllocationHelper.allocate(vv, BATCH_SIZE, 4);
+    vvMap.put("event3", vv);
+    f = MaterializedField.create(new SchemaPath("event4", ExpressionPosition.UNKNOWN), Types.required(TypeProtos.MinorType.VARCHAR));
+    vv = TypeHelper.getNewVector(f, allocator);
+    AllocationHelper.allocate(vv, BATCH_SIZE, 4);
+    vvMap.put("event4", vv);
+    f = MaterializedField.create(new SchemaPath("event5", ExpressionPosition.UNKNOWN), Types.required(TypeProtos.MinorType.VARCHAR));
+    vv = TypeHelper.getNewVector(f, allocator);
+    AllocationHelper.allocate(vv, BATCH_SIZE, 4);
+    vvMap.put("event5", vv);
 
     Map<String, HBaseFieldInfo> projs = new HashMap<>();
     projs.put("uid", rkFieldInfoMap.get("uid"));
-
+    projs.put("uhash", rkFieldInfoMap.get("uhash"));
+    projs.put("date", rkFieldInfoMap.get("date"));
+    projs.put("event0", rkFieldInfoMap.get("event0"));
+    projs.put("event1", rkFieldInfoMap.get("event1"));
+    projs.put("event2", rkFieldInfoMap.get("event2"));
+    projs.put("event3", rkFieldInfoMap.get("event3"));
+    projs.put("event4", rkFieldInfoMap.get("event4"));
+    projs.put("event5", rkFieldInfoMap.get("event5"));
+    
     byte[] rk = Bytes.toBytesBinary(rowKey);
     int index = 0;
     long cost = 0;
-    final int round = 10000000;
-    for (int i = 0; i < 100; i++) {//warm up
-      dfaRowKeyParser.parseAndSet(rk, projs, vvMap, index, true);
+    dfaRowKeyParser.parseAndSet(rk, projs, vvMap, index, true);
+    if(uid!=0){
+      Assert.assertEquals(uid, vvMap.get("uid").getAccessor().getObject(index));
     }
-    long t0 = System.nanoTime();
-    for (int i = 0; i < round; i++) {
-      dfaRowKeyParser.parseAndSet(rk, projs, vvMap, index, true);
-      if (index == BATCH_SIZE - 1) {
-        AllocationHelper.allocate(vv, BATCH_SIZE, 4);
-        index = 0;
-      }
+    if(uhash!=0){
+      Assert.assertEquals(uhash, vvMap.get("uhash").getAccessor().getObject(index));
     }
-    long t1 = System.nanoTime();
-    vv.getMutator().setValueCount(10);
-    LOG.info("Total speed: " + (int)(round/((t1-t0)/1e9)) + " line/sec");
-    ValueVector.Accessor accessor = vv.getAccessor();
-    Set<Integer> uidSet = new HashSet<>();
-    for (int i=0; i<vv.getAccessor().getValueCount(); i++) {
-      int uid = (Integer)accessor.getObject(i);
-      uidSet.add(uid);
+    if(date!=0){
+      Assert.assertEquals(date, vvMap.get("date").getAccessor().getObject(index));
     }
-    assertEquals(1, uidSet.size());
+    if(event0!=null){
+      Assert.assertEquals(event0.getBytes(), vvMap.get("event0").getAccessor().getObject(index));
+    }
+    if(event1!=null){
+      Assert.assertEquals(event1.getBytes(), vvMap.get("event1").getAccessor().getObject(index));
+    }
+    if(event2!=null){
+      Assert.assertEquals(event2.getBytes(), vvMap.get("event2").getAccessor().getObject(index));
+    }
+    if(event3!=null){
+      Assert.assertEquals(event3.getBytes(), vvMap.get("event3").getAccessor().getObject(index));
+    }
+    if(event4!=null){
+      Assert.assertEquals(event4.getBytes(), vvMap.get("event4").getAccessor().getObject(index));
+    }
+    if(event5!=null){
+      Assert.assertEquals(event5.getBytes(), vvMap.get("event5").getAccessor().getObject(index));
+    }
+  }
+  
+  @Test
+  public void testParseRowKey(){
+    String rowKey = "20130918response.agei.report.241427s.pend.2s5s.\\xFF[\\x00\\x00\\x00@"  ;
+    assertParseRowKey(rowKey, 20130918, "response", "agei", "report", "241427s", "pend", "2s5s", '[', '@');
 
   }
 
