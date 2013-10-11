79c238c7b5aa93671185167d7c62ba3d34f8ef17
witwolf <wo.bian.shi.mo0@gmail.com>
2013-10-11 16:31:06 +0800

    .

--- sandbox/prototype/common/src/test/java/org/apache/drill/common/expression/parser/TestEscapeChar.java
+++ sandbox/prototype/common/src/test/java/org/apache/drill/common/expression/parser/TestEscapeChar.java
@@ -18,13 +18,14 @@ public class TestEscapeChar {
   @Test
   public void test() throws IOException {
     ObjectMapper mapper= DrillConfig.create().getMapper();
-    String str = "COMMON,ram,2013-10-01,2013-10-02,visit.*,{\"identifier\":\"\\\"null\\\"\"},VF-ALL-0-0,PERIOD";
-    System.out.println(str);
+    String str = "COMMON,ram,2013-10-01,2013-10-02,visit.*,{\"identifier\":\"\\\\\"null\\\\\"\"},VF-ALL-0-0,PERIOD";
     System.out.println(str);
     LogicalExpression qs = new ValueExpressions.QuotedString(str, ExpressionPosition.UNKNOWN);
     String str2 = mapper.writeValueAsString(qs);
     System.out.println(str2);
-    LogicalExpression le=mapper.readValue(str2, LogicalExpression.class);
+
+    LogicalExpression le=mapper.readValue( str2 , LogicalExpression.class);
+    System.out.println(mapper.writeValueAsString(le));
     System.out.println(((ValueExpressions.QuotedString)le).value);
 
   }
