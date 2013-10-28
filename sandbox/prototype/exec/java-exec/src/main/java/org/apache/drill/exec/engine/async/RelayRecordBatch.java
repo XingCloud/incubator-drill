76de6898fa14f965432a627fac83e3983832d42d
witwolf <wo.bian.shi.mo0@gmail.com>
2013-10-28 11:06:44 +0800

    限制blockqueue的大小控制driver的执行，防止过多的数据存储在内存中

--- sandbox/prototype/exec/java-exec/src/main/java/org/apache/drill/exec/engine/async/RelayRecordBatch.java
+++ sandbox/prototype/exec/java-exec/src/main/java/org/apache/drill/exec/engine/async/RelayRecordBatch.java
@@ -3,8 +3,6 @@ package org.apache.drill.exec.engine.async;
 import org.apache.drill.exec.record.RecordBatch;
 
 public interface RelayRecordBatch extends RecordBatch {
-  void markNextFailed(RuntimeException cause);
   void mirrorAndStash(IterOutcome o);
-  void postCleanup();
-  boolean isKilled();
+  void cleanUp();
 }
