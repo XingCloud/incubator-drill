37e88e0ca71188986e1d53842995eae077d23929
witwolf <wo.bian.shi.mo0@gmail.com>
2013-10-28 12:03:06 +0800

    限制blockqueue的大小控制driver的执行，防止过多的数据存储在内存中

---
+++ sandbox/prototype/exec/java-exec/src/main/java/org/apache/drill/exec/engine/async/SimpleRelayRecordBatch.java
@@ -0,0 +1,40 @@
+package org.apache.drill.exec.engine.async;
+
+import org.apache.drill.exec.record.RecordBatch;
+
+import java.util.concurrent.LinkedBlockingDeque;
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: witwolf
+ * Date: 10/28/13
+ * Time: 9:39 AM
+ */
+
+// for common physical operator except scan & screen
+public class SimpleRelayRecordBatch extends AbstractRelayRecordBatch {
+
+  public SimpleRelayRecordBatch() {
+    recordFrames = new LinkedBlockingDeque<>();
+  }
+
+  @Override
+  protected void stash(RecordFrame recordFrame) {
+    recordFrames.add(recordFrame);
+  }
+
+  @Override
+  public IterOutcome next() {
+    current = recordFrames.poll();
+    if (current == null) {
+      return IterOutcome.NOT_YET;
+    }
+    return current.outcome;
+  }
+
+  @Override
+  public void kill() {
+    cleanUp();
+    kill();
+  }
+}
